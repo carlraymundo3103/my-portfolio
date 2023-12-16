20c2968b2bb7309550996464cc916368d424dda2
carlraymundo3103 <153609440+carlraymundo3103@users.noreply.github.com>
2023-12-16 21:19:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,111 @@
-console.clear();
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
-const loginBtn = document.getElementById('login');
-const signupBtn = document.getElementById('signup');
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  });
+}
 
-loginBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode.parentNode;
-	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			signupBtn.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 3000);
+}
+
+function handleIntersection(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+const observer = new IntersectionObserver(handleIntersection, {
+  threshold: 0.5
 });
 
-signupBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode;
-	Array.from(e.target.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			loginBtn.parentNode.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
+const revealElements = document.querySelectorAll('.reveal');
+
+revealElements.forEach(element => {
+  observer.observe(element);
+});
+
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section) {
+    var offset = section.offsetTop;
+
+    window.scrollTo({
+      top: offset
+    });
+  }
+}
+
+document.getElementById('shopNowBtn').addEventListener('click', function (event) {
+  scrollToSection('products');
+
+  var navLinks = document.querySelectorAll('#navbar li a');
+  navLinks.forEach(function (link) {
+    link.classList.remove('active');
+  });
+
+  var productsLink = document.querySelector('#navbar li:nth-child(2) a');
+  productsLink.classList.add('active');
+  event.preventDefault();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var productsLink = document.querySelector('#navbar li:nth-child(2) a'); 
+  var productsSection = document.getElementById('products');
+  var smBannerSection = document.getElementById('sm-banner');
+
+  productsLink.addEventListener('click', function () {
+   
+    var navLinks = document.querySelectorAll('#navbar li a');
+    navLinks.forEach(function (link) {
+      link.classList.remove('active');
+    });
+
+  
+    this.classList.add('active');
+    productsSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+
+  const smBannerObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        productsLink.classList.remove('active');
+      }
+    });
+  });
+
+  smBannerObserver.observe(smBannerSection);
 });
 
